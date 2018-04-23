d05484923a56f824ff126b7644298b61f962e1ad
charleyo9 <36996997+charleyo9@users.noreply.github.com>
2018-04-23 14:22:39 -0700

    Add files via upload

---
+++ Video Game/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+createCanvas (1000,600);
+background(80); // gray background
+angleMode(DEGREES); // Change the mode to DEGREES
+
+}
+
+function draw() {
+beginShape ();
+// Draw a rectangle at location (30, 20) with a width and height of 55.
+rect(30, 20, 55, 55);
+
+
+// Draw a rectangle at location (50, 30) with a width and height of 55.
+rect(30, 75, 20, 50);
+endShape();
+
+// Draw a rectangle at location (50, 30) with a width and height of 55.
+rect(65, 75, 20, 50);
+endShape();
+
+// beginShape ();
+// // Draw a rectangle at location (30, 20) with a width and height of 55.
+// rect(30, 50, 55, 55);
+// endShape();
+//
+//
+//
+//
+
+}
